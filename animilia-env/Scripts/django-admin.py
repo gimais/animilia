811a1f0df4ed0e0fbf2b1910b1040d0030fcf448
Maoj <meke1001000@gmail.com>
2020-09-25 18:23:39 +0400

    gitignore file and git rm --chached

--- animilia-env/Scripts/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!C:\Users\meke1\PycharmProjects\django2.1.1\animilia-env\Scripts\python.exe
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
